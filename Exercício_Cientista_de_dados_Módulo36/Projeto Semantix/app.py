386b56696598f7b007b12e91b0a2bdbdf9291099
Marcelo Otsuki <marceloaires7@gmail.com>
2024-02-22 19:58:56 -0300

    Mod36ProjetoSemantix

--- Exercício_Cientista_de_dados_Módulo36/Projeto Semantix/app.py
+++ Exercício_Cientista_de_dados_Módulo36/Projeto Semantix/app.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import time
 import streamlit as st
-from analise import analise
-from analise import stats
-import openai
+from formulas import analise
+from formulas import stats
 
 sns.set_theme()
 
@@ -28,11 +25,3 @@ col1, col2, col3 = st.columns(3)
 col1.bar_chart(stats(x='Result', df=df_home), color=['#ff0000','#ffaa00', '#00ff00'])
 col2.bar_chart(stats(x='Poss', df=df_home), color=['#ff0000','#ffaa00', '#00ff00'])
 col3.bar_chart(stats(x='Opponent', df=df_home), color=['#ff0000','#ffaa00', '#00ff00'])
-
-openai.api_key = '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'
-
-completion = openai.chat.completions.create(
-                                            model="gpt-3.5-turbo",
-                                            messages=[{"role": "user", "content": f'O dataset a seguir corresponde a metricas de partidas de futebol da Premier League. Me informe insights sobre este dataset {df_home}'}])
-
-st.markdown(completion.choices[0].message.content)
