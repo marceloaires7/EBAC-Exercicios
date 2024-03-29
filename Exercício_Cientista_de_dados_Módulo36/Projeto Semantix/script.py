50a77d3c882a2a565f643b05bdcbe44c871bf3dd
Marcelo Otsuki <marceloaires7@gmail.com>
2024-03-28 21:39:27 -0300

    Mod37

--- Exercício_Cientista_de_dados_Módulo36/Projeto Semantix/script.py
+++ Exercício_Cientista_de_dados_Módulo36/Projeto Semantix/script.py
@@ -18,3 +18,26 @@ def stats(x, df):
     team_stats = (pd.crosstab(columns=df['Result'], index=df[x]).sort_values(by=['Vitória', 'Empate', 'Derrota'], ascending= False))
 
     return team_stats
+
+def get_dict(df, i, coluna, dict):
+    x = df.loc[df[coluna] == i, coluna].map({valor: chave for chave, valor in dict.items()}).to_list()
+    if x == []:
+        y = df[coluna].map({valor: chave for chave, valor in dict.items()})
+        x = y.loc[y == i]
+        x = x.map(dict).to_list()
+    return x
+
+def Legenda(grafico=None, rotacao=None, titulo=None): #Fórmula para colocar legenda no gráfico.
+    
+    ax = plt.gca()  # Obter o eixo atual.
+    
+    for p in ax.patches: #for para criar a legenda.
+        ax.annotate(f'{round(p.get_height(),2)}', #Texto a ser escrito no gráfico.
+                    xy=(p.get_x() + p.get_width() / 2, p.get_height()), #Localização do Texto.
+                    ha='center', #Alinhamento Horizontal.
+                    va='center', #Alinhamento Vertical.
+                    xytext= (0,15),
+                    textcoords= 'offset points',
+                    rotation=rotacao) #Rotação do texto.
+    
+    plt.title(titulo) #Título do gráfico.
