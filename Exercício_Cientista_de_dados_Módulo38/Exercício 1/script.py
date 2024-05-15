fa905d69084874397bdf5456eed0a205bf0569ad
Marcelo Otsuki <marceloaires7@gmail.com>
2024-05-15 18:58:54 -0300

    Mod38

---
+++ Exercício_Cientista_de_dados_Módulo38/Exercício 1/script.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import accuracy_score, roc_auc_score
+from scipy.stats import t
+from scipy.stats import ks_2samp
+
+def analise(data, y):
+    analise = pd.DataFrame({'dtype': data.dtypes,
+                            'contagem': data.count(),
+                            'missing': data.isna().sum(),
+                            'nunique': data.nunique(),
+                            'papel': 'covariavel'})
+    analise.loc[analise.index == y, 'papel'] = 'resposta'
+    return analise
+
+def IV(variavel, resposta):
+    tab = pd.crosstab(variavel, resposta, margins=True, margins_name='total')
+
+    rótulo_evento = tab.columns[0]
+    rótulo_nao_evento = tab.columns[1]
+
+    tab['pct_evento'] = tab[rótulo_evento]/tab.loc['total',rótulo_evento]
+    tab['ep'] = tab[rótulo_evento]/tab.loc['total',rótulo_evento]
+    
+    tab['pct_nao_evento'] = tab[rótulo_nao_evento]/tab.loc['total',rótulo_nao_evento]
+    tab['woe'] = np.log(tab.pct_evento/tab.pct_nao_evento)
+    tab['iv_parcial'] = (tab.pct_evento - tab.pct_nao_evento)*tab.woe
+    return tab['iv_parcial'].sum()
+
+def biv_discreta(var, df):
+    df['bom'] = 1-df.mau
+    g = df.groupby(var)
+
+    biv = pd.DataFrame({'qt_bom': g['bom'].sum(),
+                        'qt_mau': g['mau'].sum(),
+                        'mau':g['mau'].mean(), 
+                        var: g['mau'].mean().index, 
+                        'cont':g[var].count()})
+    
+    biv['ep'] = (biv.mau*(1-biv.mau)/biv.cont)**.5
+    biv['mau_sup'] = biv.mau+t.ppf([0.975], biv.cont-1)*biv.ep
+    biv['mau_inf'] = biv.mau+t.ppf([0.025], biv.cont-1)*biv.ep
+    
+    biv['logit'] = np.log(biv.mau/(1-biv.mau))
+    biv['logit_sup'] = np.log(biv.mau_sup/(1-biv.mau_sup))
+    biv['logit_inf'] = np.log(biv.mau_inf/(1-biv.mau_inf))
+
+    tx_mau_geral = df.mau.mean()
+    woe_geral = np.log(df.mau.mean() / (1 - df.mau.mean()))
+
+    biv['woe'] = biv.logit - woe_geral
+    biv['woe_sup'] = biv.logit_sup - woe_geral
+    biv['woe_inf'] = biv.logit_inf - woe_geral
+
+    fig, ax = plt.subplots(2,1, figsize=(8,6))
+    ax[0].plot(biv[var], biv.woe, ':bo', label='woe')
+    ax[0].plot(biv[var], biv.woe_sup, 'o:r', label='limite superior')
+    ax[0].plot(biv[var], biv.woe_inf, 'o:r', label='limite inferior')
+    
+    num_cat = biv.shape[0]
+    ax[0].set_xlim([-.3, num_cat-.7])
+
+    ax[0].set_ylabel("Weight of Evidence")
+    ax[0].legend(bbox_to_anchor=(.83, 1.17), ncol=3)
+    
+    ax[0].set_xticks(list(range(num_cat)))
+    ax[0].set_xticklabels(biv[var], rotation=15)
+    
+    ax[1] = biv.cont.plot.bar()
+    return biv
+
+
+def biv_continua(var, ncat, df):
+    df['bom'] = 1-df.mau
+    cat_srs, bins = pd.qcut(df[var], ncat, retbins=True, precision=0, duplicates='drop')
+    g = df.groupby(cat_srs, observed=False)
+
+    biv = pd.DataFrame({'qt_bom': g['bom'].sum(),
+                        'qt_mau': g['mau'].sum(),
+                        'mau':g['mau'].mean(), 
+                        var: g[var].mean(),     
+                        'cont':g[var].count()})
+    
+    biv['ep'] = (biv.mau*(1-biv.mau)/biv.cont)**.5
+    biv['mau_sup'] = biv.mau+t.ppf([0.975], biv.cont-1)*biv.ep
+    biv['mau_inf'] = biv.mau+t.ppf([0.025], biv.cont-1)*biv.ep
+    
+    biv['logit'] = np.log(biv.mau/(1-biv.mau))
+    biv['logit_sup'] = np.log(biv.mau_sup/(1-biv.mau_sup))
+    biv['logit_inf'] = np.log(biv.mau_inf/(1-biv.mau_inf))
+
+    tx_mau_geral = df.mau.mean()
+    woe_geral = np.log(df.mau.mean() / (1 - df.mau.mean()))
+
+    biv['woe'] = biv.logit - woe_geral
+    biv['woe_sup'] = biv.logit_sup - woe_geral
+    biv['woe_inf'] = biv.logit_inf - woe_geral
+
+    fig, ax = plt.subplots(2,1, figsize=(8,6))
+    ax[0].plot(biv[var], biv.woe, ':bo', label='woe')
+    ax[0].plot(biv[var], biv.woe_sup, 'o:r', label='limite superior')
+    ax[0].plot(biv[var], biv.woe_inf, 'o:r', label='limite inferior')
+    
+    num_cat = biv.shape[0]
+
+    ax[0].set_ylabel("Weight of Evidence")
+    ax[0].legend(bbox_to_anchor=(.83, 1.17), ncol=3)
+    
+    ax[1] = biv.cont.plot.bar()
+    return biv
+
+def print_metricas(dados, PD='PD', CLASSE_PRED='classe_predita', RESP='mau'):
+
+    #Acuracia
+    acc = accuracy_score(dados[RESP], dados[CLASSE_PRED])
+
+    #AUC
+    auc = roc_auc_score(dados[RESP], dados[PD])
+
+    #Gini
+    gini = 2 * auc - 1
+
+    #KS
+    ks = ks_2samp(dados.loc[dados[RESP] == 1, PD], dados.loc[dados[RESP] != 1,
+                                                             PD]).statistic
+
+    print('KS:       {0:.2f}%'.format(ks * 100))
+    print('AUC:      {0:.2f}%'.format(auc * 100))
+    print('GINI:     {0:.2f}%'.format(gini * 100))
+    print('Acurácia: {0:.2f}%\n'.format(acc * 100))
+
+    return None
